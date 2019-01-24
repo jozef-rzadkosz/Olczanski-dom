bf4f1d8a4737d25205ca6f292177bbb1b1aaaaa4
freestyle09 <jozefrzadkosz09@gmail.com>
2019-01-24 17:31:14 +0100

    fixed startup configuration

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   state = {};
   render() {
-    return <div>App works!</div>;
+    return <div>App works! :)</div>;
   }
 }
 
