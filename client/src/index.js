5ab669926a1c5290d6231e015f526f0a5c712158
freestyle09 <jozefrzadkosz09@gmail.com>
2019-01-24 17:16:44 +0100

    finished first configuration full stack

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+//styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './styles/style.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
