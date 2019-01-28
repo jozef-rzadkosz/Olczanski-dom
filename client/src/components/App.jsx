fb084c236242e1ba62ac59d8822edbbd6781599c
freestyle09 <jozefrzadkosz09@gmail.com>
2019-01-28 19:02:11 +0100

    added gallery component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,6 +5,7 @@ import Header from './Header';
 import Home from './Home';
 import AboutUs from './AboutUs';
 import Live from './Live';
+import Gallery from './Gallery';
 
 class App extends Component {
   state = {};
@@ -15,6 +16,7 @@ class App extends Component {
         <Home />
         <AboutUs />
         <Live />
+        <Gallery />
       </div>
     );
   }
---
+++ client/src/components/Gallery.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Lightbox from './Lightbox';
+
+class Gallery extends Component {
+  images = [
+    {
+      id: 1,
+      url: 'images/1.jpg',
+      tb: 'images/tb/1.jpg'
+    },
+    {
+      id: 2,
+      url: 'images/2.jpg',
+      tb: 'images/tb/2.jpg'
+    },
+    {
+      id: 3,
+      url: 'images/3.jpg',
+      tb: 'images/tb/3.jpg'
+    },
+    {
+      id: 4,
+      url: 'images/4.jpg',
+      tb: 'images/tb/4.jpg'
+    },
+    {
+      id: 5,
+      url: 'images/5.jpg',
+      tb: 'images/tb/5.jpg'
+    }
+  ];
+  render() {
+    return (
+      <section id='gallery'>
+        <Lightbox images={this.images} />
+      </section>
+    );
+  }
+}
+
+export default Gallery;
