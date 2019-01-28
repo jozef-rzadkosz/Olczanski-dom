3f149be8f0e75f77c0b2f2a58eed9919e10e5efc
freestyle09 <jozefrzadkosz09@gmail.com>
2019-01-28 19:13:07 +0100

    finished gallery component

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -2,39 +2,35 @@ import React, { Component } from 'react';
 import Lightbox from './Lightbox';
 
 class Gallery extends Component {
-  images = [
-    {
-      id: 1,
-      url: 'images/1.jpg',
-      tb: 'images/tb/1.jpg'
-    },
-    {
-      id: 2,
-      url: 'images/2.jpg',
-      tb: 'images/tb/2.jpg'
-    },
-    {
-      id: 3,
-      url: 'images/3.jpg',
-      tb: 'images/tb/3.jpg'
-    },
-    {
-      id: 4,
-      url: 'images/4.jpg',
-      tb: 'images/tb/4.jpg'
-    },
-    {
-      id: 5,
-      url: 'images/5.jpg',
-      tb: 'images/tb/5.jpg'
-    }
-  ];
+  images = [];
+
   render() {
     return (
       <section id='gallery'>
         <Lightbox images={this.images} />
       </section>
     );
+  }
+
+  componentWillMount() {
+    for (let i = 1; i < 24; i++) {
+      let obj = {};
+      if (i === 7) {
+        obj = {
+          id: i,
+          url: `images/${i}.jpg`,
+          tb: `images/tb/${i}.jpg`,
+          class: `vertical`
+        };
+      } else {
+        obj = {
+          id: i,
+          url: `images/${i}.jpg`,
+          tb: `images/tb/${i}.jpg`
+        };
+      }
+      this.images.push(obj);
+    }
   }
 }
 
