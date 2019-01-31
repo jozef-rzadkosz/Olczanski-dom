855784a5742fa80cbe1a0971f3e872669c4b1af4
freestyle09 <jozefrzadkosz09@gmail.com>
2019-01-31 13:41:59 +0100

    removed home path

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const request = require('request');
+
 const app = express();
 
 const contact = require('./routes/contact');
@@ -8,10 +8,6 @@ const weather = require('./routes/weather');
 app.use(express.json());
 app.use('/contact', contact);
 app.use('/weather', weather);
-
-app.get('/', (req, res) => {
-  res.send('Hello world, server works! :)');
-});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
