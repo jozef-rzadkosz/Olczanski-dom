09a35a5777171887ee52fb480b58508f9e8d0e0a
freestyle09 <jozefrzadkosz09@gmail.com>
2019-02-01 13:06:56 +0100

    fixed footer icon

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -18,16 +18,16 @@ class Footer extends Component {
             href='https://www.booking.com/hotel/pl/olczanski-dom.pl.html?label=gen173nr-1DCAEoggJCAlhYSDNiBW5vcmVmaLYBiAEBmAEeuAEMyAEM2AED6AEBqAID;sid=18707b473eae14656f18eb6faeae384f;dest_id=-538221;dest_type=city;dist=0;group_adults=2;hpos=1;room1=A%2CA;sb_price_type=total;srfid=10f670d2dc87fddcf12efae33a78294101f36e75X1;type=total;ucfs=1&'
             target='_blank'
           >
-            <img src='booking.png' alt='' />
+            <img className='booking' src='booking.png' alt='' />
           </a>
           <a
             href='https://www.google.com/search?source=hp&ei=FPJSXMj_HtHMwAKt3IvYCQ&q=olcza%C5%84ski+dom&btnK=Szukaj+w+Google&oq=olcza%C5%84ski+dom&gs_l=psy-ab.3..35i39j0i22i30.164.2211..2377...2.0..1.214.1786.0j13j1......0....1..gws-wiz.....0..0i131j0i67j0i10j0.w4iTbGyhOds'
             target='_blank'
           >
-            <i className='fab fa-google-plus-square' />
+            <img src='Google_plus_icon.svg' alt='' />
           </a>
           <a href='https://www.facebook.com/olczanskidom/' target='_blank'>
-            <i className='fab fa-facebook' />
+            <img src='facebook.png' alt='' />
           </a>
         </div>
       </footer>
